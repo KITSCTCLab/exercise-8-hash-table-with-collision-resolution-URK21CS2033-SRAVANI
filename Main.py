c2022e8efa46a94e50b9d0ad7081be2a28274557
URK21CS2033-SRAVANI <109648319+URK21CS2033-SRAVANI@users.noreply.github.com>
2022-10-07 19:13:48 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,20 @@
 import re
+
 def display_hash(hashtable) -> None:
-for keyvalue in range(len(hashtable)):
-content = hashtable[keyvalue]
-print(keyvalue, end = " ")
-for element in content:
-print("-->", element, end = " ")
-print()
+	for keyvalue in range(len(hashtable)):
+		content = hashtable[keyvalue]
+		print(keyvalue, end = " ")
+		for element in content:
+			print("-->", element, end = " ")
+		print()
 
 def Hashing(keyvalue) -> int:
-return keyvalue % len(HashTable)
+	return keyvalue % len(HashTable)
 
 def insert(Hashtable, keyvalue, value) -> None:
-Hashtable[Hashing(keyvalue)].append(value)
+	Hashtable[Hashing(keyvalue)].append(value)
+
+
 # Do not edit the following code
 hash_table_size = int(input())
 # Create Hashtable as a list of list.
@@ -19,8 +22,9 @@ HashTable = [[] for _ in range(hash_table_size)]
 input_data = input()
 data = []
 for item in re.split('], |].', input_data):
-item = item[1:]
-data = item.split(', ')
-if len(data) > 1:
-insert(HashTable, int(data[0]), data[1])
+  item = item[1:]
+  data = item.split(', ')
+  if len(data) > 1:
+    insert(HashTable, int(data[0]), data[1])
+
 display_hash(HashTable)
